68e2290b7ffc73f7a7bb59de478d5ee28ba988d5
TomTalbot <104102909+TomTalbot@users.noreply.github.com>
2022-09-20 18:22:25 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,74 +1,91 @@
 import pygame
-import pygame_menu
-import random
-import PyQt5
-import menu
+from pygame.locals import *
 import os
-from sys import exit
-from pygame import font
-import button
-
-
-
-'''
-initial ideas:
-side scrolling 2 mario style platformer with procedural generation, obstacles and potentially enemies?
-implementing PyQt5 as pygame menus are a ball-ache. First have pyqt5 for menu and what not, when ready the player
-selects a play option which deinitialises the PyQt5 library and enables Pygame.
-
-
-dev log 1 8/9/22:
-deciding to use pygame menus, due to the freedom and customisability - potentially?
-'''
+import math
 
 pygame.init()
-pygame.mixer.init()
+
+clock = pygame.time.Clock()
+FPS = 60
+
+SCREENWIDTH = 1000
+SCREENHEIGHT = 406
 
 
-WIN = pygame.display.set_mode((400,400))
-pygame.display.set_caption('Unraveling worlds?')
-
-#game vars
-game_paused = False
+WIN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+pygame.display.set_caption('platformer')
 
 
-#font defining
-
-FONT = pygame.font.SysFont('comicsans',40)
-
-#colours
-TEXT_COL = (255,255,255)
+# draw stuff
+BG_img = pygame.image.load(os.path.join('Assets', 'BG.png'))
+BG_width = BG_img.get_width()
 
 
-def draw_text(text,font,text_col,x,y):
-    img = font.render(text, True, text_col)
-    WIN.blit(img, (x,y))
+# def game variables
 
-
+# math.ceil rounds number up, tile width calculates the number of tiles needed to be blitted to fill screen
+tile_width = math.ceil(SCREENWIDTH / BG_width)
 
 
 
-#game loop
-while True:
-    if game_paused == True:
-        pass #dispaly menu
-    else:
-
-        draw_text("MENU",FONT,TEXT_COL, 135,150)
-
-
-#evnt handler
-    for event in pygame.event.get():
-
-        if event.type ==pygame.KEYDOWN:
-
-            if event.key == pygame.K_SPACE:
-                game_paused = True
-
-        if event.type == pygame.QUIT:
-            quit()
+class player():
+    def __init__(self, x,y):
+        pimg = pygame.image.load(os.path.join('Assets', 'player.png'))
+        self.image = pygame.transform.scale(pimg, (80,80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.grav = 0
 
 
+
+    def movement(self):
+
+        dx = 0 # when key is pressed, shows the change (delta)
+        dy = 0
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            self.grav = -15 # neg value increase y coord
+        if keys[pygame.K_a]:
+            dx -= 5
+        if keys [pygame.K_d]:
+            dx += 5
+
+
+        self.grav += 1
+        if self.grav > 10: #if y >10 then it gets capped and player starts falling
+            
+            self.grav = 10
+
+        dy += self.grav
+
+
+        #updating coords
+        self.rect.x += dx
+        self.rect.y += dy
+
+
+        WIN.blit(self.image,self.rect)
+
+
+player = player(100,300)
+
+
+run = True
+while run:
+
+    clock.tick(FPS)
+
+    # blit imgs
+    for i in range(0, tile_width):
+        WIN.blit(BG_img, (i * BG_width, 0))
+
+    player.movement()
+
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            run = False
 
     pygame.display.update()
 
